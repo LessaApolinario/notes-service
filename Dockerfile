99ba6ac2d850130c6c033a39be816c49c0d46f6b
Lessa Apolinario <lessaapolinario@gmail.com>
2026-03-16 21:58:58 -0300

    Add initial prisma setup

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+ FROM node:24-alpine
+
+WORKDIR /app
+
+COPY package.json ./
+COPY yarn.lock ./
+
+RUN yarn cache clean --force
+RUN yarn install --legacy-peer-deps
+
+COPY . .
+
+RUN npx prisma generate
+
+RUN yarn build
+
+EXPOSE 3000
+
+CMD ["yarn", "start:migrate:prod"]
---
+++ src/infra/env/env.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  DATABASE_URL: z.url(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  POSTGRES_HOST_PORT: z.coerce.number(),
+  PORT: z.coerce.number().optional().default(3333),
+});
+
+export type Env = z.infer<typeof envSchema>;
